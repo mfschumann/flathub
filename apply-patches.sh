674281e041f4a3eb6e06ed8f1c584251be09fbb1
Martin Schumann <mfs@mfs.name>
2022-08-27 23:35:21 +0200

    patch -i

--- apply-patches.sh
+++ apply-patches.sh
@@ -35,11 +35,11 @@ echo "Applying patch series for main repository"
 echo "... without patches for Windows installer"
 sed -i 's/08-branding-m-c.patch/# 08-branding-m-c.patch/g' thunderbird-patches/$VERSION/series-M-C
 sed -i 's/08a-branding-m-c.patch/# 08a-branding-m-c.patch/g' thunderbird-patches/$VERSION/series-M-C
-cat thunderbird-patches/$VERSION/series-M-C | while read line || [[ -n $line ]]; do [[ -f patches/$line ]] && echo Applying patch $line ... && patch -R -p1 < patches/$line; done
+cat thunderbird-patches/$VERSION/series-M-C | while read line || [[ -n $line ]]; do [[ -f patches/$line ]] && echo Applying patch $line ... && patch -R -p1 -i patches/$line; done
 
 echo
 echo "======================================================="
 echo "Applying patch series for comm repository"
 cd comm
-cat thunderbird-patches/$VERSION/series | while read line || [[ -n $line ]]; do [[ -f ../patches/$line ]] && echo Applying patch $line ... && patch -R -p1 < ../patches/$line; done
+cat thunderbird-patches/$VERSION/series | while read line || [[ -n $line ]]; do [[ -f ../patches/$line ]] && echo Applying patch $line ... && patch -R -p1 -i ../patches/$line; done
 cd ..
