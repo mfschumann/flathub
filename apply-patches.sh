5a12129ff5d19027ff4d89001bda7ebbeaa28ec0
Martin Schumann <mfs@mfs.name>
2022-08-28 21:07:18 +0200

    adapted mozconfigs for flatpak

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
 VERSION="$1"
 
-UNAME=$(uname)
-UNAME_ARCH=$(uname -i)
-# Hack for Debian 11 Bullseye on Amazon EC2 image.
-if [[ "$UNAME" == *"$aarch64"* ]] && [ "$UNAME_ARCH" = "unknown" ]; then
-    UNAME_ARCH="aarch64"
-fi
-
-. thunderbird-patches/$VERSION/$VERSION.sh
-
-if [ "$UNAME_ARCH" = "x86_64" ]; then
-    echo
-    echo "======================================================="
-    echo "Copying mozconfig-Linux"
-    cp thunderbird-patches/$VERSION/mozconfig-Linux mozconfig
-elif [ "$UNAME_ARCH" = "aarch64" ]; then
-    echo
-    echo "======================================================="
-    echo "Copying mozconfig-Linux-aarch64"
-    cp thunderbird-patches/$VERSION/mozconfig-Linux-aarch64 mozconfig
-fi
-
 echo
 echo "======================================================="
 echo "Copying patches"
-cp thunderbird-patches/$VERSION/mozconfig-Linux mozconfig
 rm -rf patches; mkdir patches
 find thunderbird-patches/$VERSION -type f -name *.patch -exec cp '{}' patches ';'
 
